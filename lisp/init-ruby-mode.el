e946f4213f062ea79794a2845a4a389bcd6833ff
Tiankai <xiongtk@gmail.com>
2019-04-08 09:52:38 +0800

    initial commit

---
+++ lisp/init-ruby-mode.el
@@ -0,0 +1,22 @@
+;; -*- coding: utf-8; lexical-binding: t; -*-
+
+(add-auto-mode 'ruby-mode
+               "\\.\\(rb\\|rake\\|rxml\\|rjs\\|irbrc\\|builder\\|ru\\|gemspec\\)\\'"
+               "\\(Rakefile\\|Gemfile\\)\\'")
+
+(setq ruby-use-encoding-map nil)
+
+(defun ruby-mode-hook-setup ()
+  (unless (is-buffer-file-temp)
+    (robe-mode)
+    (push 'company-robe company-backends)
+    (setq-local compile-command "rake ")
+    (flymake-ruby-load)))
+(add-hook 'ruby-mode-hook 'ruby-mode-hook-setup)
+
+;; Following generic project tools are more useful:
+;; - find-file-in-project
+;; - counsel-git-grep
+;; - compile
+
+(provide 'init-ruby-mode)
